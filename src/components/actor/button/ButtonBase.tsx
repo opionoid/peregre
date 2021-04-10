e1c953e40535faf6b6db3ca36f03116704754c66
Opion <KindledKindred@gmail.com>
2021-04-10 16:56:21 +0900

    css: ButtonBase

--- src/components/actor/button/ButtonBase.tsx
+++ src/components/actor/button/ButtonBase.tsx
@@ -21,4 +21,14 @@ const ButtonBaseWrapper = styled.button`
   background-color: inherit;
   color: ${color.font};
   padding: ${space.xs};
+  border-radius: 8px;
+  border: 0;
+  outline: 0;
+  transition: box-shadow ease-out 0.3s;
+  box-shadow:  4px 4px 12px ${color.backgroundDarkShadowDarken},
+                 -4px -4px 12px ${color.backgroundDarkShadowLighten};
+  &:active {
+    box-shadow: inset 4px 4px 12px ${color.backgroundDarkShadowDarken},
+                inset -4px -4px 12px ${color.backgroundDarkShadowLighten};
+  }
 `
