fa0c911bc170f201eb5ab2598760408de5c62ddb
Opion <KindledKindred@gmail.com>
2021-04-11 17:34:20 +0900

    data: ability と profession

---
+++ src/data/index.ts
@@ -0,0 +1,5 @@
+// TODO: 今は直に書いてるけど firebase なり spreadsheet なりに置いて graphQL 叩く
+import { ABILITY_LIST } from './ability'
+import { PROFESSION_LIST } from './profession'
+
+export { ABILITY_LIST, PROFESSION_LIST }
---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 汎用的な interface の定義場所
+ * 増えてきたらファイルを分ける
+ */
+export interface IImage {
+  src: string;
+  alt: string;
+}
