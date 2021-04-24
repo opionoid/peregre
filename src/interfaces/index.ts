6afecf521314d77bdacd1beeea4dc02e4de476b3
Opion <KindledKindred@gmail.com>
2021-04-24 13:04:54 +0900

    add: interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,6 +3,34 @@
  * 増えてきたらファイルを分ける
  */
 export interface IImage {
-  src: string;
-  alt: string;
+  src: string
+  alt?: string // 画像近くの文が画像と同じ内容のときのみ空になる
+}
+
+export interface ILink {
+  href: string
+  target?: string
+  text?: string
+}
+
+export interface IAbility {
+  name: string
+  icon: IImage
+  text: string
+  level: number
+}
+
+export interface IWeapon {
+  name: string
+  description: string
+  hp: number
+  skillList: ISkill[]
+}
+
+export interface ISkill {
+  name: string
+  description: string
+  depth: number
+  shouldCast: boolean
+  isUlt: boolean
 }
