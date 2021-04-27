028fd759da547ca77da956308fe1328a3f8c2b41
Opion <kindledkindred@gmail.com>
2021-04-27 20:42:56 +0900

    delete: デフォのApp.tsx

--- src/pages/character.tsx
+++ src/pages/character.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { RecoilRoot } from 'recoil'
+import { color } from 'src/assets/style'
 import { CharacterMaking } from 'src/components/character/CharacterMaking'
 import { CharacterSheet } from 'src/components/character/CharacterSheet'
+import styled from 'styled-components'
 //import styled from 'styled-components'
 
 export interface ICharacterPageProps {}
 
 export const CharacterPage: React.VFC = () => {
   return (
-    <RecoilRoot>
-      <CharacterMaking />
-      <CharacterSheet />
-    </RecoilRoot>
+    <Page>
+      <RecoilRoot>
+        <CharacterMaking />
+        <CharacterSheet />
+      </RecoilRoot>
+    </Page>
   )
 }
+
+const Page = styled.div`
+  background-color: ${color.background};
+`
 
 /**
  * config（デフォルトは全てオフ）
