134f41f1a703600938c738f0d9f2c2671f1e3e12
Opion <KindledKindred@gmail.com>
2021-11-13 15:19:09 +0900

    style: icon更新

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,10 +3,12 @@ import Agitation from './Agitation.svg'
 import AncientKnowledge from './AncientKnowledge.svg'
 import Astrology from './Astrology.svg'
 import Bird from './Bird.svg'
+import BowOfTheElfKing from './BowOfTheElfKing.svg'
 import Bribery from './Bribery.svg'
 import Cheating from './Cheating.svg'
 import Depth from './Depth.svg'
 import Dissociative from './Dissociative.svg'
+import DragonRod from './DragonRod.png'
 import Drink from './Drink.svg'
 import Enlightenment from './Enlightenment.svg'
 import FirstAid from './FirstAid.svg'
@@ -17,6 +19,7 @@ import LockPicking from './LockPicking.svg'
 import MalleusMaleficarum from './MalleusMaleficarum.svg'
 import MaxDepth from './MaxDepth.svg'
 import MaxHp from './MaxHp.svg'
+import MotherNightKnife from './MotherNightKnife.svg'
 import Narrative from './Narrative.svg'
 import Navigation from './Navigation.svg'
 import Negotiation from './Negotiation.svg'
@@ -26,8 +29,10 @@ import Paste from './Paste.svg'
 import Performance from './Performance.svg'
 import Perfuming from './Perfuming.svg'
 import Philosophy from './Philosophy.svg'
+import PilotLantern from './PilotLantern.svg'
 import Robust from './Robust.svg'
 import Save from './Save.svg'
+import SpearOfTheCavalryKing from './SpearOfTheCavalryKing.svg'
 import Theology from './Theology.svg'
 import Torture from './Torture.svg'
 import Training from './Training.svg'
@@ -39,10 +44,12 @@ export const Icons = {
   AncientKnowledge,
   Astrology,
   Bird,
+  BowOfTheElfKing,
   Bribery,
   Cheating,
   Depth,
   Dissociative,
+  DragonRod,
   Drink,
   Enlightenment,
   FirstAid,
@@ -53,6 +60,7 @@ export const Icons = {
   MalleusMaleficarum,
   MaxDepth,
   MaxHp,
+  MotherNightKnife,
   Narrative,
   Navigation,
   Negotiation,
@@ -62,8 +70,10 @@ export const Icons = {
   Performance,
   Perfuming,
   Philosophy,
+  PilotLantern,
   Robust,
   Save,
+  SpearOfTheCavalryKing,
   Theology,
   Torture,
   Training,
