ecd96300367bbc78f063c190d7746a23c580e381
Opion <KindledKindred@gmail.com>
2021-11-24 01:34:02 +0900

    fix: safari での NPC 深度アイコンの表示崩れ

--- src/components/stories/StoryNpc.tsx
+++ src/components/stories/StoryNpc.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { useToggle } from 'react-use'
 import { Icons } from 'src/assets/icons'
@@ -20,34 +19,50 @@ export const StoryNpc: React.VFC<IStoryNpcProps> = (props) => {
 
   const { onClickSkill } = useBattle()
   const [depth, setDepth] = React.useState<0 | 1 | 2 | 3 | 4>(2)
-  const handleClick = (skill: ISkill) => onClickSkill(skill, setDepth, props.name)
+  const handleClick = (skill: ISkill) =>
+    onClickSkill(skill, setDepth, props.name)
 
   return (
     <StoryNpcWrapper>
       <Info>
         <h4>{props.name}</h4>
         <Status>
-          <label aria-label="魂魄量"><Icon src={Icons.MaxHp} /></label>
+          <label aria-label="魂魄量">
+            <Icon src={Icons.MaxHp} />
+          </label>
           <HpInput value={hp} onChange={(e) => setHp(Number(e.target.value))} />
         </Status>
         <Status>
-          <label aria-label="魂魄量"><Icon src={Icons.MaxDepth} /></label>
+          <label aria-label="魂魄量">
+            <Icon src={Icons.MaxDepth} />
+          </label>
           <span>{depth}</span>
         </Status>
         <ToggleButton>
-          <ButtonBase aria-controls="skills" onClick={() => toggleShown()}>{shouldShowSkills ? 'スキル非表示' : 'スキル表示'}</ButtonBase>
+          <ButtonBase aria-controls="skills" onClick={() => toggleShown()}>
+            {shouldShowSkills ? 'スキル非表示' : 'スキル表示'}
+          </ButtonBase>
         </ToggleButton>
       </Info>
       <div id="skills" aria-hidden={!shouldShowSkills}>
-        {props.skills.map(skill => (
+        {props.skills.map((skill) => (
           <Skill key={skill.name}>
             <SkillRow>
               <SkillName>{skill.name}</SkillName>
-              <SkillDepth><Icon src={Icons.MaxDepth} />{skill.depth}</SkillDepth>
+              <SkillDepth>
+                <Icon src={Icons.MaxDepth} />
+                {skill.depth}
+              </SkillDepth>
             </SkillRow>
             <SkillDescription>{skill.description}</SkillDescription>
             <SkillButton>
-              <ButtonBase onClick={() => handleClick({ ...skill, icon: { src: '' }, isUlt: false })}>使用</ButtonBase>
+              <ButtonBase
+                onClick={() =>
+                  handleClick({ ...skill, icon: { src: '' }, isUlt: false })
+                }
+              >
+                使用
+              </ButtonBase>
             </SkillButton>
           </Skill>
         ))}
@@ -58,55 +73,56 @@ export const StoryNpc: React.VFC<IStoryNpcProps> = (props) => {
 
 const StoryNpcWrapper = styled.div``
 const Info = styled.div`
-display: flex;
-align-items: center;
-height: fit-content;
-flex-wrap: wrap;
-& > *:not(:last-child) {
-  margin-right: ${space.s};
-}
+  display: flex;
+  align-items: center;
+  height: fit-content;
+  flex-wrap: wrap;
+  & > *:not(:last-child) {
+    margin-right: ${space.s};
+  }
 `
 const Status = styled.div`
-display: flex;
-align-items: center;
+  display: flex;
+  align-items: center;
 `
 const Icon = styled.img`
-width: 1.6rem;
-margin-right: ${space.xxs};
+  width: 1.6rem;
+  height: 1.6rem;
+  margin-right: ${space.xxs};
 `
 const HpInput = styled.input``
 const ToggleButton = styled.div`
-width: 10rem;
-height: 1.2rem;
+  width: 10rem;
+  height: 1.2rem;
 `
 const Skill = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-flex-wrap: wrap;
-& > * {
-  margin-top: ${space.s};
-}
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  & > * {
+    margin-top: ${space.s};
+  }
 `
 const SkillRow = styled.div`
-display: flex;
-align-items: center;
-justify-content: space-between;
-min-width: 14rem;
-flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  min-width: 14rem;
+  flex-shrink: 0;
 `
 const SkillName = styled.div`
-margin-right: ${space.xs};
+  margin-right: ${space.xs};
 `
 const SkillDepth = styled.div`
-display: flex;
-flex-shrink: 0;
+  display: flex;
+  flex-shrink: 0;
 `
 const SkillDescription = styled.p`
-margin: ${space.s};
-margin-bottom: 0;
+  margin: ${space.s};
+  margin-bottom: 0;
 `
 const SkillButton = styled.div`
-width: 8rem;
-flex-shrink: 0;
+  width: 8rem;
+  flex-shrink: 0;
 `
